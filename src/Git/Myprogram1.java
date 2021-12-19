1b59a57b1428c4522320b635c2b274d61a36adbc
ADMIN <ADMIN@DESKTOP-M68ANQG>
2021-12-19 23:27:02 +0530

    I am commiting first time

---
+++ src/Git/Myprogram1.java
@@ -0,0 +1,12 @@
+package Git;
+
+public class Myprogram1 {
+int val=10;
+	public static void main(String[] args) {
+		//System.out.println("GOOD MORNING");
+		//System.out.println("Velocity");
+Myprogram1 obj=new Myprogram1();
+System.out.println(obj.val);
+	}
+
+}
